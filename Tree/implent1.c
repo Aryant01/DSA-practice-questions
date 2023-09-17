d135e6363b2c9b7f8cf9d1e89202181563113a47
Aryant Kumar <aryantkumar0101@gmail.com>
2023-09-17 13:22:14 +0530

    tree-implementation

---
+++ Tree/implent1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct treenode {
+    int value;
+    struct treenode *left;
+    struct treenode *right;
+} treenode;
+
+treenode *createNode(int value){
+    treenode* result = malloc(sizeof(treenode));
+
+    if (result != NULL) {
+        result->value = value;
+        result->left = NULL;
+        result->right = NULL;
+    }
+    return result;
+}
+
+void printtabs(int numtabs) {
+    for (int i=0; i < numtabs; i++) {
+        printf("\t");
+    }
+}
+
+void printtree_rec(treenode *root, int level) {
+    if (root == NULL) {
+        printf("---<empty>---\n");
+        return;
+    }
+
+    printtabs(level);
+    printf("value = %d\n", root->value);
+    printtabs(level);
+    printf("left\n");
+
+    printtree_rec(root->left, level+1);
+    printtabs(level);
+    printf("right\n");
+
+    printtree_rec(root->right, level+1);
+
+    printtabs(level);
+    printf("done\n");
+}
+
+void printtree(treenode *root) {
+    printtree_rec(root, 0);
+}
+
+int main() {
+    treenode *n1 = createNode(10);
+    treenode *n2 = createNode(11);
+    treenode *n3 = createNode(10);
+    treenode *n4 = createNode(12);
+    treenode *n5 = createNode(14);
+
+    n1->left = n2;
+    n1->right = n3;
+    n3->left = n4;
+    n3->right = n5;
+
+    printtree(n1);    
+    
+    free(n1);
+    free(n2);
+    free(n3);
+    free(n4);
+    free(n5);
+
+}
