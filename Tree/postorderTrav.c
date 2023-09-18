69bb8894cb83fb0dc58cd522c154823c682f2eea
Aryant Kumar <aryantkumar0101@gmail.com>
2023-09-18 18:05:31 +0530

    postorder-tree-traversal

---
+++ Tree/postorderTrav.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+typedef struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+} node;
+ 
+node* newNode(int data) {
+    node* node = malloc(sizeof(node));
+
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+void printPostorder(node* node) {
+    if (node == NULL)
+        return;
+
+    // First recur on left subtree
+    printPostorder(node->left);
+
+    // Then recur on right subtree
+    printPostorder(node->right);
+
+    // Now deal with the node
+    printf("%d ", node->data);
+} 
+
+int main() {
+    node* root = newNode(1);
+
+    root->left = newNode(2);
+    root->right = newNode(3);
+
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    //Function call
+    printf("Postorder traversal of binary tree is \n");
+    printPostorder(root);
+
+    getchar();
+    return 0;
+}
